f494afc8ca1d754fee9cb522e845c4a3e2e7f1e2
kamyarkamali <kamyarkamali021@gmail.com>
2024-09-09 17:47:31 +0330

    started custom hook useTitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 
+//Custom hook
+import { useTitle } from "./hooks/useTitle";
+
+//types/enums
+import { NAMETITLEPROJECT } from "./types/enum/enums";
+
 function App() {
+  useEffect(() => {
+    useTitle(NAMETITLEPROJECT.titlePage);
+  }, []);
+
   return <></>;
 }
 
---
+++ src/types/enum/enums.ts
@@ -0,0 +1,3 @@
+export enum NAMETITLEPROJECT {
+  titlePage = "تسک شرکت ریرا",
+}
